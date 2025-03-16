31772c06f81e1e2b97366d277c37413a4bda70fd
Vitaliy Svyatodukh <v.sviatodukh@m2e.team>
2025-03-16 19:06:40 +0200

    css code highlighting

--- resources/views/demo.php
+++ resources/views/demo.php
@@ -1,29 +1,33 @@
 <link rel="stylesheet" href="<?php echo $home; ?>/assets/css/usage.css">
 
+<script src="<?php echo $home; ?>/assets/highlight/js/highlight.min.js"></script>
+<link rel="stylesheet" href="<?php echo $home; ?>/assets/highlight/css/atom-one-dark.min.css">
+<script>hljs.highlightAll();</script>
+
 <div class="container content">
     <h1 class="text-center py-3">How to use Swift Framework?</h1>
     <p>Swift Framework is built on the MVC concept. The entry point of the application is <code>public/index.php</code>.</p>
 
     <h2 class="subtitle">Configuration</h2>
     <p>Application settings are defined in <code>config/app.php</code>:</p>
-    <div class="code-block">
-        &lt;?php
-        const APPLICATION = 'D:/xampp/htdocs/framework_test/';
+    <pre><code class="language-php">
+    &lt;?php
+    const APPLICATION = 'D:/xampp/htdocs/framework_test/';
 
-        const HOST = 'http://localhost';
-        const DOMAIN_SYM = false;
-        const DOMAIN_ADDITION = '/framework_test';
+    const HOST = 'http://localhost';
+    const DOMAIN_SYM = false;
+    const DOMAIN_ADDITION = '/framework_test';
 
-        const DB_HOST = 'localhost';
-        const DB_USER = 'root';
-        const DB_PASS = '';
-        const DB_NAME = 'demo';
-    </div>
+    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+    const DB_NAME = 'demo';
+    </code></pre>
 
     <h2 class="subtitle">Routing</h2>
     <p>Routes are defined in <code>config/routes.php</code>:</p>
-    <div class="code-block">
-        &lt;?php
+    <pre><code class="language-php">
+     &lt?php
         use app\Controllers\MainPageController;
         use app\Controllers\DemoController;
 
@@ -37,11 +41,11 @@
                 'action' => 'index'
             ]
         ];
-    </div>
+    </code></pre>
 
     <h2 class="subtitle">Controllers</h2>
     <p>Controllers extend the base <code>Controller</code> class and are located in <code>app/Controllers</code>:</p>
-    <div class="code-block">
+    <pre><code class="language-php">
         &lt;?php
 
         namespace app\Controllers;
@@ -63,11 +67,11 @@
                 $this->render('footer');
             }
         }
-    </div>
+    </code></pre>
 
     <h2 class="subtitle">ORM</h2>
     <p>The built-in ORM allows working with tables as objects:</p>
-    <div class="code-block">
+    <pre><code class="language-php">
         &lt;?php
 
         namespace app\Models;
@@ -79,5 +83,6 @@
             */
             protected $table = 'movie';
         }
-    </div>
+    </code></pre>
 </div>
+
